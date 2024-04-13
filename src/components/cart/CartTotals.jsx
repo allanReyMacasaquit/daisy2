62948677f913b9bdd803390fe9c87992e9455213
allan <allanrey.macasaquit@gmail.com>
2024-04-13 16:46:57 -0400

    cart totals

--- src/components/cart/CartTotals.jsx
+++ src/components/cart/CartTotals.jsx
@@ -1,4 +1,30 @@
+import { useSelector } from 'react-redux';
+import { formatPrice } from '../../utils';
+
 const CartTotals = () => {
-	return <div>CartTotals</div>;
+	const { cartTotal, shipping, tax, orderTotal } = useSelector(
+		(state) => state.cartState
+	);
+	const carts = {
+		cartTotal,
+		shipping,
+		tax,
+		orderTotal,
+	};
+
+	return (
+		<div className='card bg-base-300 capitalize'>
+			<div className='card-body '>
+				{Object.keys(carts).map((key) => (
+					<p key={key} className='flex justify-between pb-2'>
+						<span>{key}</span>
+						<span className='font-medium tracking-widest'>
+							{formatPrice(carts[key])}
+						</span>
+					</p>
+				))}
+			</div>
+		</div>
+	);
 };
 export default CartTotals;
